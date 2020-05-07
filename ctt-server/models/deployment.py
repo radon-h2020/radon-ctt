d58c98f05de03c3fcb977bfabcdb7b48dcec40f1
Thomas F. Düllmann <duellmann@iste.uni-stuttgart.de>
2020-05-07 13:12:11 +0200

    #57 Added flag to drop policies before deployment and its implementation.

--- ctt-server/models/deployment.py
+++ ctt-server/models/deployment.py
@@ -1,6 +1,8 @@
+import glob
 import os
 import subprocess
 import uuid
+import yaml
 import zipfile
 
 from flask import current_app
@@ -9,7 +11,7 @@ from sqlalchemy import Column, String, ForeignKey
 from db_orm.database import Base, db_session
 from models.testartifact import TestArtifact
 from models.abstract_model import AbstractModel
-from util.configuration import BasePath
+from util.configuration import BasePath, DropPolicies
 
 
 class Deployment(Base, AbstractModel):
@@ -42,19 +44,21 @@ class Deployment(Base, AbstractModel):
 
     def deploy(self):
         test_artifact = TestArtifact.get_by_uuid(self.testartifact_uuid)
-        sut_fq_path = os.path.join(test_artifact.fq_storage_path, test_artifact.sut_tosca_path)
-        ti_fq_path = os.path.join(test_artifact.fq_storage_path, test_artifact.ti_tosca_path)
+        sut_csar_path = os.path.join(test_artifact.fq_storage_path, test_artifact.sut_tosca_path)
+        ti_csar_path = os.path.join(test_artifact.fq_storage_path, test_artifact.ti_tosca_path)
 
         tosca_meta_file_path = 'TOSCA-Metadata/TOSCA.meta'
 
         # Deployment of SuT
-        if os.path.isfile(sut_fq_path):
+        if os.path.isfile(sut_csar_path):
             os.makedirs(self.sut_storage_path)
 
             # As soon as opera supports automatic extraction,
             # we do not need the extraction step anymore
-            zipfile.ZipFile(sut_fq_path).extractall(self.sut_storage_path)
-            entry_definition = Deployment.get_csar_entry_definition(os.path.join(self.sut_storage_path, tosca_meta_file_path))
+            zipfile.ZipFile(sut_csar_path).extractall(self.sut_storage_path)
+            entry_definition = Deployment.get_csar_entry_definition(
+                os.path.join(self.sut_storage_path, tosca_meta_file_path))
+            Deployment.drop_policies(self.sut_storage_path)
             #
 
             current_app.logger.debug(f'Deploying SuT {str(entry_definition)} with opera '
@@ -62,13 +66,15 @@ class Deployment(Base, AbstractModel):
             subprocess.call(['opera', 'deploy', entry_definition], cwd=self.sut_storage_path)
 
         # Deployment of TI
-        if os.path.isfile(ti_fq_path):
+        if os.path.isfile(ti_csar_path):
             os.makedirs(self.ti_storage_path)
 
             # As soon as opera supports automatic extraction,
             # we do not need the extraction step anymore
-            zipfile.ZipFile(ti_fq_path).extractall(self.ti_storage_path)
-            entry_definition = Deployment.get_csar_entry_definition(os.path.join(self.ti_storage_path, tosca_meta_file_path))
+            zipfile.ZipFile(ti_csar_path).extractall(self.ti_storage_path)
+            entry_definition = Deployment.get_csar_entry_definition(
+                os.path.join(self.ti_storage_path, tosca_meta_file_path))
+            Deployment.drop_policies(self.ti_storage_path)
             #
 
             if entry_definition:
@@ -99,6 +105,34 @@ class Deployment(Base, AbstractModel):
                     if line.startswith('Entry-Definitions: '):
                         return line.strip().split(' ')[1]
         raise Exception('Entry definition could not be found.')
+
+    @classmethod
+    def drop_policies(cls, folder):
+        """Removes any occurrences of policy_type and policies-elements in the *.tosca files in the given folder."""
+
+        # Drop policies enabled in the configuration?
+        if DropPolicies:
+            definitions_folder = os.path.join(folder, "_definitions")
+            if os.path.isdir(folder) and os.path.isdir(definitions_folder):
+                file_list = glob.glob(definitions_folder + os.sep + "*.tosca")
+                if len(file_list) > 0:
+                    for file in file_list:
+                        with open(file, 'r') as tosca_file_in:
+                            tosca_yaml = yaml.full_load(tosca_file_in)
+
+                        if 'policy_types' in tosca_yaml:
+                            del tosca_yaml['policy_types']
+                        if 'topology_template' in tosca_yaml and 'policies' in tosca_yaml['topology_template']:
+                            del tosca_yaml['topology_template']['policies']
+
+                        with open(file, 'w') as tosca_file_out:
+                            yaml.dump(tosca_yaml, tosca_file_out)
+                    # current_app.logger.info("Processed", len(file_list), "tosca-files with 'drop_policies'.")
+                else:
+                    # traceback.print_stack(..)
+                    current_app.logger.info("No tosca-files in", definitions_folder)
+        else:
+            current_app.logger.info("DropPolicies disabled, skipping.")
 
     @classmethod
     def get_parent_type(cls):
